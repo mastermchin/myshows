c90dba437981d73aed998204e15619311c7d6ce4
gobozov <gobozov@gmail.com>
2013-10-01 12:45:04 +0400

    appcompat

--- src/ru/myshows/activity/MainActivity.java
+++ src/ru/myshows/activity/MainActivity.java
@@ -17,8 +17,10 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import ru.myshows.adapters.EpisodesAdapter;
 import ru.myshows.adapters.TabsAdapter;
 import ru.myshows.domain.Searchable;
 import ru.myshows.fragments.*;
@@ -35,9 +37,9 @@ public class MainActivity extends MenuActivity {
 
     private ViewPager pager;
     private PagerTabStrip pagerTabStrip;
-    private TabsAdapter adapter;
-    private EditText search;
-    private Bundle savedInstanceState;
+    private EpisodesAdapter adapter;
+   // private EditText search;
+   // private Bundle savedInstanceState;
 
 
     @Override
@@ -49,13 +51,14 @@ public class MainActivity extends MenuActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.main);
-        this.savedInstanceState = savedInstanceState;
+       // this.savedInstanceState = savedInstanceState;
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 
-        adapter = new TabsAdapter(getSupportFragmentManager(), false);
+        //adapter = new TabsAdapter(getSupportFragmentManager(), false);
+
         pager = (ViewPager) findViewById(R.id.pager);
-        pager.setOffscreenPageLimit(6);
-        pager.setAdapter(adapter);
+        pager.setOffscreenPageLimit(2);
+       // pager.setAdapter(adapter);
         pagerTabStrip = (PagerTabStrip) findViewById(R.id.pagerTabStrip);
         pagerTabStrip.setTabIndicatorColorResource(R.color.light_red);
 
@@ -72,15 +75,19 @@ public class MainActivity extends MenuActivity {
 
 
     public boolean onCreateOptionsMenu(Menu menu) {
-
         menu.add(0, 1, 1, R.string.menu_update).setIcon(R.drawable.ic_navigation_refresh).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-        menu.add(0, 2, 2, R.string.menu_settings).setIcon(R.drawable.ic_action_settings).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-        menu.add(0, 3, 3, R.string.menu_search).setIcon(R.drawable.ic_action_search).setActionView(R.layout.action_search).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
+//        menu.add(0, 2, 2, R.string.menu_settings).setIcon(R.drawable.ic_action_settings).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+//        menu.add(0, 3, 3, R.string.menu_search).setIcon(R.drawable.ic_action_search).setActionView(R.layout.action_search).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW);
         menu.add(0, 4, 4, R.string.menu_exit).setIcon(R.drawable.ic_exit).setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-
         return super.onCreateOptionsMenu(menu);
     }
 
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(1).setVisible(MyShows.isLoggedIn);
+        return super.onPrepareOptionsMenu(menu);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -91,21 +98,22 @@ public class MainActivity extends MenuActivity {
                 if (!MyShows.isLoggedIn)
                     break;
 
-                Fragment currentFragment = adapter.getItem(position);
+                //Fragment currentFragment = adapter.getItem(position);
+                Fragment currentFragment = getFragment(position);
                 if (currentFragment != null)
                     ((Taskable) currentFragment).executeUpdateTask();
 
                 break;
-            case 2:
-                startActivity(new Intent(this, SettingsAcrivity.class));
-                break;
-            case 3:
-                search = (EditText) item.getActionView();
-                search.addTextChangedListener(filterTextWatcher);
-                search.requestFocus();
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-                break;
+//            case 2:
+//                startActivity(new Intent(this, SettingsAcrivity.class));
+//                break;
+//            case 3:
+//                search = (EditText) item.getActionView();
+//                //search.addTextChangedListener(filterTextWatcher);
+//                search.requestFocus();
+//                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//                imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+//                break;
             case 4:
                 final AlertDialog alert;
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this)
@@ -130,77 +138,77 @@ public class MainActivity extends MenuActivity {
         return true;
     }
 
-    private TextWatcher filterTextWatcher = new TextWatcher() {
-        public void afterTextChanged(Editable s) {
-        }
+//    private TextWatcher filterTextWatcher = new TextWatcher() {
+//        public void afterTextChanged(Editable s) {
+//        }
+//
+//        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//        }
+//
+//        public void onTextChanged(CharSequence s, int start, int before, int count) {
+//            Fragment fragment = getFragment(pager.getCurrentItem());
+//            if (fragment instanceof Searchable) {
+//                ((Searchable) fragment).getAdapter().getFilter().filter(s);
+//            }
+//        }
+//
+//    };
+//
+//    private void getPrivateTabs() {
+//
+//        if (savedInstanceState == null) {
+//
+//            adapter.addFragment(new ShowsFragment(ShowsFragment.SHOWS_USER), getResources().getString(R.string.tab_shows_title));
+//            adapter.addFragment(new NewEpisodesFragment(), getResources().getString(R.string.tab_new));
+//            if (Settings.getBoolean(Settings.PREF_SHOW_NEXT))
+//                adapter.addFragment(new NextEpisodesFragment(), getResources().getString(R.string.tab_next));
+//            if (Settings.getBoolean(Settings.PREF_SHOW_NEWS))
+//                adapter.addFragment(new NewsFragment(), getResources().getString(R.string.tab_news_title));
+//            if (Settings.getBoolean(Settings.PREF_SHOW_PROFILE))
+//                adapter.addFragment(new ProfileFragment(), getResources().getString(R.string.tab_profile_title));
+//            adapter.addFragment(new SearchFragment(), getResources().getString(R.string.tab_search_title));
+//
+//        } else {
+//            Integer count = savedInstanceState.getInt("tabsCount");
+//            String[] titles = savedInstanceState.getStringArray("titles");
+//            for (int i = 0; i < count; i++) {
+//                adapter.addFragment(getFragment(i), titles[i]);
+//            }
+//        }
+//
+//        //indicator.notifyDataSetChanged();
+//        adapter.notifyDataSetChanged();
+//
+//        if (savedInstanceState != null)
+//            pager.setCurrentItem(savedInstanceState.getInt("currentTab"));
+//
+//
+//    }
 
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        }
-
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-            Fragment fragment = getFragment(pager.getCurrentItem());
-            if (fragment instanceof Searchable) {
-                ((Searchable) fragment).getAdapter().getFilter().filter(s);
-            }
-        }
-
-    };
-
-    private void getPrivateTabs() {
-
-        if (savedInstanceState == null) {
-
-            adapter.addFragment(new ShowsFragment(ShowsFragment.SHOWS_USER), getResources().getString(R.string.tab_shows_title));
-            adapter.addFragment(new NewEpisodesFragment(), getResources().getString(R.string.tab_new));
-            if (Settings.getBoolean(Settings.PREF_SHOW_NEXT))
-                adapter.addFragment(new NextEpisodesFragment(), getResources().getString(R.string.tab_next));
-            if (Settings.getBoolean(Settings.PREF_SHOW_NEWS))
-                adapter.addFragment(new NewsFragment(), getResources().getString(R.string.tab_news_title));
-            if (Settings.getBoolean(Settings.PREF_SHOW_PROFILE))
-                adapter.addFragment(new ProfileFragment(), getResources().getString(R.string.tab_profile_title));
-            adapter.addFragment(new SearchFragment(), getResources().getString(R.string.tab_search_title));
-
-        } else {
-            Integer count = savedInstanceState.getInt("tabsCount");
-            String[] titles = savedInstanceState.getStringArray("titles");
-            for (int i = 0; i < count; i++) {
-                adapter.addFragment(getFragment(i), titles[i]);
-            }
-        }
-
-        //indicator.notifyDataSetChanged();
-        adapter.notifyDataSetChanged();
-
-        if (savedInstanceState != null)
-            pager.setCurrentItem(savedInstanceState.getInt("currentTab"));
-
-
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        if (adapter != null) {
-            outState.putInt("tabsCount", adapter.getCount());
-            List<String> titles = adapter.getTitles();
-            outState.putStringArray("titles", titles.toArray(new String[titles.size()]));
-        }
-        if (pager != null)
-            outState.putInt("currentTab", pager.getCurrentItem());
-    }
-
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        if (adapter != null) {
+//            outState.putInt("tabsCount", adapter.getCount());
+//            List<String> titles = adapter.getTitles();
+//            outState.putStringArray("titles", titles.toArray(new String[titles.size()]));
+//        }
+//        if (pager != null)
+//            outState.putInt("currentTab", pager.getCurrentItem());
+//    }
+//
     private Fragment getFragment(int position) {
-        return savedInstanceState == null ? adapter.getItem(position) : getSupportFragmentManager().findFragmentByTag(getFragmentTag(position));
+        return  getSupportFragmentManager().findFragmentByTag(getFragmentTag(position));
     }
-
+//
     private String getFragmentTag(int position) {
         return "android:switcher:" + R.id.pager + ":" + position;
     }
-
-    private void getPublicTabs() {
-        adapter.addFragment(new LoginFragment(), getResources().getString(R.string.tab_login_title));
-        adapter.notifyDataSetChanged();
-    }
+//
+//    private void getPublicTabs() {
+//        adapter.addFragment(new LoginFragment(), getResources().getString(R.string.tab_login_title));
+//        adapter.notifyDataSetChanged();
+//    }
 
 
     private class LoginTask extends AsyncTask<Object, Void, Boolean> {
@@ -220,16 +228,25 @@ public class MainActivity extends MenuActivity {
         @Override
         protected void onPostExecute(Boolean result) {
 
-            Fragment fragment = null;
-
+            //Fragment fragment = null;
+            invalidateOptionsMenu();
             if (result) {
-                getPrivateTabs();
-                fragment = new NewEpisodesFragment();
+               // getPrivateTabs();
+                //fragment = new NewEpisodesFragment();
+                adapter = new EpisodesAdapter(MainActivity.this, getSupportFragmentManager());
+                pager.setAdapter(adapter);
                 setupDrawer();
             } else {
-                getPublicTabs();
-                setupDrawer();
-                fragment = new LoginFragment();
+                //getPublicTabs();
+
+                //setupDrawer();
+                //fragment = new LoginFragment();
+
+                pager.setVisibility(View.GONE);
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                // fragmentManager.beginTransaction().add(R.id.main, fragment).commit();
+                  fragmentManager.beginTransaction().add(R.id.main, new LoginFragment()).commitAllowingStateLoss();
+
             }
 
            // FragmentManager fragmentManager = getSupportFragmentManager();
@@ -241,20 +258,20 @@ public class MainActivity extends MenuActivity {
     }
 
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (MyShows.isLoggedIn && MyShows.isUserShowsChanged) {
-
-            Fragment showsFragment = getFragment(0);
-            if (showsFragment != null)
-                ((Taskable) showsFragment).executeTask();
-
-
-            Fragment newEpisodesFragment = getFragment(1);
-            if (newEpisodesFragment != null)
-                ((Taskable) newEpisodesFragment).executeUpdateTask();
-
-        }
-    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        if (MyShows.isLoggedIn && MyShows.isUserShowsChanged) {
+//
+//            Fragment showsFragment = getFragment(0);
+//            if (showsFragment != null)
+//                ((Taskable) showsFragment).executeTask();
+//
+//
+//            Fragment newEpisodesFragment = getFragment(1);
+//            if (newEpisodesFragment != null)
+//                ((Taskable) newEpisodesFragment).executeUpdateTask();
+//
+//        }
+//    }
 }

d396d48eb72ea057c0f01f021da0b64ac19225f3
gobozov <gobozov@gmail.com>
2013-10-02 20:03:57 +0400

    shows adapter

---
+++ src/ru/myshows/adapters/ShowAdapter.java
@@ -0,0 +1,53 @@
+package ru.myshows.adapters;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import ru.myshows.activity.R;
+import ru.myshows.fragments.NewEpisodesFragment;
+import ru.myshows.fragments.NextEpisodesFragment;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dell
+ * Date: 10/1/13
+ * Time: 1:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShowAdapter extends FragmentPagerAdapter {
+
+    private String[] titles;
+
+    public ShowAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        titles = context.getResources().getStringArray(R.array.episodes_titles);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+
+        switch (position){
+            case 0:
+                fragment = new NewEpisodesFragment();
+                break;
+            case 1:
+                fragment = new NextEpisodesFragment();
+                break;
+        }
+
+        return fragment;
+    }
+
+    public CharSequence getPageTitle(int position) {
+        if (titles != null)
+            return titles[position];
+        return "";
+    }
+
+}
